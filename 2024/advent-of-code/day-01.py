6d2d0891d540f7ca099aa5f34b26b54ab7e1e93f
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-12-03 20:34:36 -0300

    part 2 and second star

--- 2024/advent-of-code/day-01.py
+++ 2024/advent-of-code/day-01.py
@@ -13,7 +13,18 @@ def sum_input(input_file):
         return sum(abs(x - y) for x, y in zip(left, right))
 
 
-print("\nResult:", sum_input("day-01-input.txt"), "\n")
+def similarity_score(input_file):
+    with open(input_file, "r") as f:
+        left, right = [], []
+        for line in f:
+            left.append(int(line.split()[0]))
+            right.append(int(line.split()[1]))
+
+        result = 0
+        for x in left:
+            result += x * right.count(x)
+
+        return result
 
 
 class TestAll(unittest.TestCase):
@@ -24,6 +35,14 @@ class TestAll(unittest.TestCase):
     def test_result(self):
         sum = sum_input("day-01-input.txt")
         self.assertEqual(sum, 2580760)
+
+    def test_similarity_score(self):
+        score = similarity_score("day-01-input-test.txt")
+        self.assertEqual(score, 31)
+    
+    def test_similarity_score_result(self):
+        score = similarity_score("day-01-input.txt")
+        self.assertEqual(score, 25358365)
 
 
 if __name__ == "__main__":
